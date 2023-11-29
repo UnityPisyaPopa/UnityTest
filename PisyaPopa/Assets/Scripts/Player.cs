d8805123670007b33b8b2853541fafb817a6fb01
arshutz <shutoffartem@gmail.com>
2023-11-29 19:35:52 +0200

    character movement

---
+++ PisyaPopa/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+    private bool facingRight = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if (!facingRight && moveInput.x > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && moveInput.x < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
