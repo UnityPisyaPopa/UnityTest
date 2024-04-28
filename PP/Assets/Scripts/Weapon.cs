e9abc2ee91b95cca2ec40a13ad0de89f4c531507
arshutz <shutoffartem@gmail.com>
2024-04-28 20:46:01 +0300

    ---

--- PP/Assets/Scripts/Weapon.cs
+++ PP/Assets/Scripts/Weapon.cs
@@ -6,10 +6,8 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] private Sprite sawedOff;
     [SerializeField] private Sprite sawedOff_reload;
-    [SerializeField] private Sprite sawedOff_fire;
 
     [SerializeField] private GameObject projectile;
-    [SerializeField] private GameObject muzzleFlash;
     [SerializeField] private Transform shotPoint;
 
     [SerializeField] private float reloadStart;
@@ -67,9 +65,9 @@ public class Weapon : MonoBehaviour
     IEnumerator MuzzleFlash()
     {
         shotPoint.GetComponent<SpriteRenderer>().enabled = true;
-        muzzleFlash.GetComponent<Light2D>().enabled = true;
+        shotPoint.GetComponent<Light2D>().enabled = true;
         yield return new WaitForSeconds(0.08f);
         shotPoint.GetComponent<SpriteRenderer>().enabled = false;
-        muzzleFlash.GetComponent<Light2D>().enabled = false;
+        shotPoint.GetComponent<Light2D>().enabled = false;
     }
 }
